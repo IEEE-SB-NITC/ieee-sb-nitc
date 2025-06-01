dd8668440efd8c2e62d60c400cbd94bbe5772210
simonM21 <simonmattekkatt21@gmail.com>
2025-06-01 21:31:10 +0530

    took out the nav bar

--- app/components/OurTeamHeader.jsx
+++ app/components/OurTeamHeader.jsx
@@ -4,27 +4,6 @@ import styles from "./OurTeamHearder.module.css";
 function Header() {
   return (
     <div className={styles.header}>
-      <div className={styles.main}>
-        <div>
-          <Image
-            className={styles.logo}
-            src="/assets/logo.png"
-            alt="Logo"
-            width={100}
-            height={70}
-          />
-          {/* <img className="logo" src={logo} alt="" /> */}
-        </div>
-        <div className={styles.items}>
-          <button className={styles.btn}>Home</button>
-          <button className={styles.btn}>Chapters</button>
-          <button className={styles.btn}>Execom</button>
-          <button className={styles.btn}>Gallery</button>
-          <button className={styles.btn}>Contact Us</button>
-          <button className={styles.btn}>Blogs</button>
-          <button className={styles.btn}>Join IEEE</button>
-        </div>
-      </div>
       <div className={styles.ieee}>
         <span className={styles.ieee1}>IEEE SB NITC</span>
         <span className={styles.ieee2}>EXECOM 2025-26</span>
