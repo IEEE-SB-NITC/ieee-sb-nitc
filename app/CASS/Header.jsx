eced05d63264e45f31b3585511b2dd641340e1e6
r7thu <rithuu4@gmail.com>
2025-06-10 22:52:45 +0530

    made header

--- app/CASS/Header.jsx
+++ app/CASS/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from "next/image";
+
 
 const Header = () => {
   return (
     <div className="Header">
+      <div className="main">
+            <div >
+                <Image className="logo" src="/assets/logo.png" alt="Logo" width={100} height={70} />
+                {/* <img className="logo" src={logo} alt="" /> */}
+            </div>
+            <div className="items">
+                <button className='header-button'>Home</button>
+                <button className='header-button'>Chapters</button>
+                <button className='header-button'>Execom</button>
+                <button className='header-button'>Gallery</button>
+                <button className='header-button'>Contact Us</button>
+                <button className='header-button'>Blogs</button>
+                <button className='header-button'>Join IEEE</button>
+            </div>
+        </div>
         <div>Chapters</div>
         <div className="sub"> 
             <p>Circuit And Systems Society</p>
--- app/CASS/Section.jsx
+++ app/CASS/Section.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Section = ({text1,text2}) => {
   return (
     <div className='SectionComponent'>
-        <div className="back">
-        </div>
+        {/* <div className="back">
+        </div> */}
         <div className="front">
           <div className='fleex-col'>
           {/* <p className='dots'>..........................</p> */}
